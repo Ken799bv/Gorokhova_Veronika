43e32e997096c8b8c872e4458f523cf879e68760
Ken799bv <mvi811@yandex.ru>
2022-09-27 20:14:32 +0400

    test

---
+++ main.py
@@ -0,0 +1,55 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+"""
+a = 5
+b = 4
+c = 4
+
+print(a+b+c)
+"""
+""""
+a = 100000
+b = 60000
+c = 7000
+
+print (a-b-c)
+"""
+"""""
+print("To be", "," "or not", "," "to be ", sep=" ")
+"""""
+"""""
+print("To be",)
+print("or not",)
+print("to be")
+""""""
+"""""
+print("Life is what happens",)
+print("when",)
+print("you’re busy making other plans")
+"""""
+
+a = 10
+b = 6
+
+print (a*b)\2
+
+
+num1 = 2
+num2 = 1
+    while true:
+    print ("Hi")
